1b7960010e9e66a83df868d18bfbb80ea1fb270d
Artem Razumov <airplaneless@github.com>
2018-01-07 12:10:46 +0500

    fix

--- BotTools/interface.py
+++ BotTools/interface.py
@@ -25,7 +25,7 @@ class Interface:
 
         logging.basicConfig(
             filename=log_file,
-            level=logging.DEBUG,
+            level=logging.INFO,
             format='%(asctime)s %(message)s'
         )
 
@@ -35,31 +35,31 @@ class Interface:
     @staticmethod
     def start(bot, update):
         user = update.message.from_user
-        logging.debug('Saying hi to {}'.format(usr2str(user)))
+        logging.info('Saying hi to {}'.format(usr2str(user)))
         bot.send_message(chat_id=update.message.chat_id, text="Send stock name, which you wish find")
 
     @staticmethod
     def echo(bot, update):
         user = update.message.from_user
-        logging.debug("Repeat after user {}, text: u'{}'".format(usr2str(user), update.message.text))
+        logging.info("Repeat after user {}, text: u'{}'".format(usr2str(user), update.message.text))
         bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
 
     def rand_quote(self, bot, update):
         user = update.message.from_user
-        logging.debug('Sending quotation for {}'.format(usr2str(user)))
+        logging.info('Sending quotation for {}'.format(usr2str(user)))
         bot.send_message(chat_id=update.message.chat_id, text=self.quoter.random_quote())
 
     def search_stock(self, bot, update):
         user = update.message.from_user
         request = update.message.text
-        logging.debug('{} request search'.format(usr2str(user)))
-        logging.debug('Searching for {} stock info'.format(request))
+        logging.info('{} request search'.format(usr2str(user)))
+        logging.info('Searching for {} stock info'.format(request))
         match = self.stock_updater.find_match(request)
         if match is None:
-            logging.debug("Couldn't  find {} stock info".format(request))
+            logging.warning("Couldn't  find {} stock info".format(request))
             bot.send_message(chat_id=update.message.chat_id, text="Can't find that")
         else:
-            logging.debug("{} stock info founded".format(request))
+            logging.info("{} stock info founded".format(request))
             #Stocks.data_frame2png(match[:19], os.path.join(ROOT, 'workspace/stocks/table.html'))
             bot.send_message(chat_id=update.message.chat_id, text="For your request I found:")
             bot.send_message(
@@ -71,20 +71,20 @@ class Interface:
         user = update.message.from_user
         request = int(update.message.text.split('_')[-1])
         ticker = self.stock_updater.stock_list.iloc[request]['Ticker']
-        logging.debug("{} request load data".format(usr2str(user)))
+        logging.info("{} request load data".format(usr2str(user)))
         bot.send_message(chat_id=update.message.chat_id, text="Loading data, wait a minute")
-        logging.debug("Loading stock data for {}".format(ticker))
+        logging.info("Loading stock data for {}".format(ticker))
         try:
             data = self.stock_updater.get_stock_data(ticker)
-            logging.debug("Got {} stock data, plotting...".format(ticker))
+            logging.info("Got {} stock data, plotting...".format(ticker))
             self.stock_updater.plot_data(data, ticker, os.path.join(ROOT, 'workspace/stocks/stocks.png'))
-            logging.debug("Finish plotting, sending photo")
+            logging.info("Finish plotting, sending photo")
             bot.send_photo(
                 chat_id=update.message.chat_id,
                 photo=open(os.path.join(ROOT, 'workspace/stocks/stocks.png'), 'rb')
             )
         except RemoteDataError:
-            logging.debug("Error: Can't load data for {}".format(ticker))
+            logging.warning("Error: Can't load data for {}".format(ticker))
             bot.send_message(
                 chat_id=update.message.chat_id,
                 text="Error: Unable get data from finance.yahoo.com for {}".format(ticker)
